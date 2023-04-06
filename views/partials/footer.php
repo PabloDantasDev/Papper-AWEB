d3997fdff1cd400d1b43b2e86d33f1918472198d
Fernando Figueira <fflopesneto@hotmail.com>
2023-04-05 23:01:41 -0300

    atualização index

---
+++ views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<div class="text-center">
+		<p>
+			Site papper <a href="https://portal.uniasselvi.com.br/">UNIASSELVI</a>, feito pelos <a href="#">Alunos(as)</a>.
+		</p>
+	</div>
+</footer>
