ae99f0dbc49e9c8065dddb4c7207cf2cc4d93d54
Pablo Henrique Dantas <83853353+PabloDantasDev@users.noreply.github.com>
2023-04-05 22:35:38 -0300

    Refatoração de codigo tornando mais legivel e limpo, separando front end do back end em arquivos separados

---
+++ database/reservaM.php
@@ -0,0 +1,26 @@
+<?php
+require('conn.php');
+
+// Verifica se o formulário de cadastro foi enviado
+if (isset($_POST['reserva'])) {
+	$nMesa = strip_tags($_POST['nMesa']);
+	$nomeReserva = strip_tags($_POST['nomeReserva']);
+	$qtdPessoas = strip_tags($_POST['qtdPessoas']);
+	$telReserva = strip_tags($_POST['telReserva']);
+
+    // Insere o novo usuário no banco de dados
+    $sql = "INSERT INTO registro_mesas (num_mesa,nome_reserva,qtd_pessoas,tel_reserva) VALUES ('$nMesa', '$nomeReserva', '$qtdPessoas', '$telReserva')";
+    if ($conn->query($sql) === TRUE) {
+      echo "Usuário cadastrado com sucesso!";
+	  
+    } else {
+      echo "Erro ao cadastrar usuário: ";
+    }
+  }
+
+
+
+
+
+
+?>
