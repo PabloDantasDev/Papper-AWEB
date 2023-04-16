09315ac3c3461487bef07ab8bf2d345fa969d0cc
fabiosrs <fabiosrs@hotmail.com>
2023-04-16 20:52:50 -0300

    página SOBRE

--- views/partials/header.php
+++ views/partials/header.php
@@ -13,7 +13,7 @@
 						<a class="nav-link" href="/">Home</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#sobre">Sobre</a>
+						<a class="nav-link" href="views\sobre.php">Sobre</a>
 					</li>
 					<li class="nav-item">
 						<a id="servicosbtn" class="nav-link" href="#servicos">Serviços</a>
---
+++ views/sobre.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/sobre.css">
+    <title> Sobre </title>
+</head>
+<body>
+    <div class="container">
+        <!-- HEADER -->
+        <section class="row ">
+            <div class="col-md-6">
+                <img src="../img/garcon.jpg" class="text-center" id="garcon" alt="" srcset="">
+            </div>
+            <div class="col-md-6">
+                <h1 class="text-right">Desde 1998</h1>
+                <p class="text-justify descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elementum risus id eros egestas placerat. Maecenas est nibh, blandit nec consequat id, vehicula at odio. Quisque nec lorem libero. Duis maximus vel sem ut laoreet. Sed elementum magna eget ipsum efficitur, eu venenatis magna interdum. Phasellus bibendum tortor diam, et convallis tellus pulvinar in. Vestibulum malesuada enim augue, a sollicitudin leo varius ac. Suspendisse venenatis placerat ipsum ultricies vehicula. Nulla aliquet magna ac rhoncus condimentum. Cras quam turpis, facilisis ac molestie et, luctus id urna. Nunc vulputate quam lorem, non luctus elit malesuada id.</p>
+            </div>
+        </section>
+        <!-- CARDS -->
+        <h2>Venha viver momentos!</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/familia.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Família</h2>
+                </article>
+            </div>
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/aniversario.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Aniversário</h2>
+                </article>
+            </div>
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/trabalho.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Trabalho</h2>
+                </article>
+            </div>
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/amigos.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Amigos</h2>
+                </article>
+            </div>
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/casamento.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Casamento</h2>
+                </article>
+            </div>
+            <div class="col-md-4 card-momento text-center">
+                <article>
+                    <figure>
+                        <img src="../img/musica.jpg" alt="">
+                    </figure>
+                    <h2 class="texto-momento">Música</h2>
+                </article>
+            </div>
+        </div>
+        <!--  -->
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
